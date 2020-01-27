4a6170b88816fd89b02d25688f3ec929f7fc5fa4
keertan-krishnandraup <keertan.krishnan@draup.com>
2020-01-27 12:00:50 +0530

    all apis

--- apis.py
+++ apis.py
@@ -125,3 +125,106 @@ def add_comment():
             i['comments'].append(new_comment)
             master_coll.update_one({'posts.id':post_data['postId']},{'$set':{'posts.$.comments':i['comments']}})
     return 'Comment Added'
+
+@app.route('/get_photo', methods = ['GET'])
+def get_photo():
+    photo_id = int(request.args.get('photoId'))
+    master_coll = db['master']
+    resp = loads(dumps(master_coll.find_one({"albums.photos.id":photo_id})))
+    for i in resp['albums']:
+        for j in i['photos']:
+            if(j['id']==photo_id):
+                return dumps({'Album ID':j['albumId'],'Title':j['title'],'URL':j['title'],'Post ID':j['postId']}, indent = 4)
+    return str([])
+
+@app.route('get_all_photos', methods = ['GET'])
+def get_all_photos():
+    album_id = int(request.args.get('albumId'))
+    master_coll = db['master']
+    resp = loads(dumps(master_coll.find_one({"albums.id":album_id})))
+    photos = []
+    for i in resp['albums']:
+        for j in i['photos']:
+            photos.append({'Album ID':j['albumId'],'Title':j['title'],'URL':j['title'],'Post ID':j['postId']})
+    return dumps(photos, indent = 4)
+
+@app.route('albums', methods = ['GET'])
+def get_all_albums():
+    user_id = int(request.args.get('userId'))
+    master_coll = db['master']
+    resp = loads(dumps(master_coll.find_one({"id":user_id})))
+    albums = []
+    for i in resp['albums']:
+        albums.append({'User ID':i['userId'],'Title':i['title']})
+    return dumps(albums, indent = 4)
+
+@app.route('todos', methods = ['GET'])
+def get_todos():
+    user_id = int(request.args.get('userId'))
+    master_coll = db['master']
+    resp = loads(dumps(master_coll.find_one("id":user_id)))
+    todos = []
+    for i in resp['todos']:
+        todos.append({'User ID':i['userId'], 'ID':i['id'], 'Title':i['title'], 'Completed':i['completed']})
+    return dumps(todos, indent = 4)
+
+@app.route('create_album', methods = ['POST'])
+def create_album():
+    #_id, userId, id, title, photos, date_modified
+    post_data = request.get_json()
+    new_album = {'_id':ObjectId(),'userId':post_data['userId'],'id':post_data['id'],'title':post_data['title'],'date_modified':post_data['date_modified'],'photos':[]}
+    resp = loads(dumps(master_coll.find_one({"id":post_data['userId']})))
+    if(not resp):
+        return 'ID not found'
+    resp['albums'].append(new_album)
+    master_coll.update_one({'id':post_data['userId']}, {'$set':{'albums':resp['albums']}})
+    return 'New Album Created'
+
+@app.route('create_photo', methods = ['POST'])
+def create_photo():
+    #_id, albumId, id, title, url, thumbnailURl, date_modified, postId
+    post_data = request.get_json()
+    master_coll = db['master']
+    resp1 = master_coll.find_one({"albums.id":post_data['albumId']})
+    if(not resp1):
+        return 'Album ID not valid'
+    resp2 = master_coll.find_one({"posts.id":post_data['postId']})
+    if(not resp2):
+        return 'Post ID not valid'
+    new_photo = {'_id':ObjectId(), 'albumId':post_data['albumId'], 'id':post_data['id'], 'title':post_data['title'], 'url':post_data['url'], 'thumbnailUrl':post_data['thumbnailUrl'], 'date_modified':datetime.now(),'postId', post_data['postId']}
+    for i in resp1['albums']:
+        if(i['id']==post_data['albumId']):
+            i['photos'].append(new_photo)
+            master_coll.update_one({'albums.id':post_data['albumId']}, {'$set':{'albums.$.photos':i['photos']}})
+            break
+    return 'New Photo Created'
+
+@app.route('del_album/<int:idi>', methods = ['DELETE'])
+def del_album(idi):
+    master_coll = db['master']
+    resp = master_coll.find_one({"albums.id":idi})
+    if(not resp):
+        return 'ID not valid'
+    idx = None
+    for i in range(len(resp['albums'])):
+        if(resp['albums'][i]['id']==idi):
+            idx = i
+    resp['albums'].pop(idx)
+    master_coll.update_one({'albums.id':idi}, {'$set':{"albums":resp['albums']}})
+
+@app.route('del_photo/<int:idi>', methods = ['DELETE'])
+def del_photo(idi):
+    master_coll = db['master']
+    resp = master_coll.find_one({"albums.photos.id":idi})
+    if(not resp):
+        return 'ID not valid'
+    idx = None
+    for i in resp['albums']:
+        for j in range(len(i['photos'])):
+            if(i['photos'][j]['id']==idi):
+                idx = j
+        if(idx):
+            i['photos'].pop(idx)
+            master_coll.update_one({"albums.photos.id":idi},{'$set':{"albums.$.photos":i['photos']}})
+            break
+    return 'Deleted'
